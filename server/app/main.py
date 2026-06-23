d7c258579f6cbf2396bcb488313b429eedb3e604
Trung Hieu <hieuanthanh99@gmail.com>
2026-06-23 18:32:52 +0700

    chore: add vercel config

--- server/app/main.py
+++ server/app/main.py
@@ -41,4 +41,4 @@ def read_root():
     }
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=5000, reload=True)
